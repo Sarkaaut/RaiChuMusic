f8efda0801ae1c134d2d8a58e5b71ee752546e3d
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-03-09 17:34:59 +0300

    Update Broadcast.py

--- RaiChu/Player/Broadcast.py
+++ RaiChu/Player/Broadcast.py
@@ -14,17 +14,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Starting broadcast...`")
+        wtf = await message.reply("`دەستپێکردنی پەخش...`")
         if not message.reply_to_message:
-            await wtf.edit("Please reply to a message to start broadcast!")
+            await wtf.edit("تکایە وەڵامی نامەیەک بدەنەوە بۆ دەستپێکردنی پەخش!")
             return
         lmao = message.reply_to_message.text
         async for dialog in Ufo.iter_dialogs():
             try:
                 await Ufo.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Broadcasting...` \n\n**Sent to:** `{sent}` chats \n**Failed in:** {failed} chats")
+                await wtf.edit(f"`پەخش کردن...` \n\n** نێردراوە بۆ:** `{sent}` chats \n** شکستی هێنا لە:** {failed} chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`Gcast succesfully` \n\n**Sent to:** `{sent}` chats \n**Failed in:** {failed} chats")
+        await message.reply_text(f"`  بە سەرکەوتوویی كارا كرا` \n\n** نێردراوە بۆ:** `{sent}` chats \n** شکستی هێنا لە:** {failed} chats")
